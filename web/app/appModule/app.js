36edd1ab5c03c66c37e4d19cadaeab79dee1b31c
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-04-18 19:10:51 +0530

    New Website

---
+++ web/app/appModule/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var GMApp = angular.module('GMApp', ['ngRoute', 'ngMaterial','ui.bootstrap', 'ngResource', 'jlareau.pnotify', 'ipCookie','ui.router', 'ui.router.state.events', 'nvd3', 'ngMaterialDatePicker', 'ui.select2', 'mdColorPicker', 'ui.multiselect'])
+.controller('mainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    
+}]);
