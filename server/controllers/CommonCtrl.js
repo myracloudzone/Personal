e831b3d3bbf7f1a32e9942ba4edf5a5ebe28faa9
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-05-08 23:47:25 +0530

    Email

--- server/controllers/CommonCtrl.js
+++ server/controllers/CommonCtrl.js
@@ -7,7 +7,12 @@ var mailer = require('../lib/Mailer.js');
 module.exports = function (app) {
 	var controller = {};
 	controller.sendEmail = function(req, res, next) {
-		mailer.sendMail(req.body.to, req.body.subject, req.body.message, function() {
+		var data = "<b>Name</b> : "+req.body.name;
+		data = data + "<br><b>Email</b> : "+req.body.email;
+		data = data + "<br><b>Mobile</b> : "+req.body.mobile;
+		data = data + "<br><b>Subject</b> : "+req.body.subject;
+		data = data + "<br><b>Message</b> : <p>"+req.body.subject+"</p>";
+		mailer.sendMail(req.body.to, req.body.subject, data, function() {
 			res.send("Sent Successfully.");
 		});
     }
