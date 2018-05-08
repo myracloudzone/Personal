31813db2e5464ae6d12888c24c0004fb29bd0cb7
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-05-08 23:18:33 +0530

    New Server

--- web/app/assets/angular/js/controllers/ContactCtrl.js
+++ web/app/assets/angular/js/controllers/ContactCtrl.js
@@ -1,9 +1,23 @@
-var ContactCtrl = GMApp.controller('ContactCtrl', ['$scope', '$rootScope', '$stateParams', '$filter', '$state','$location', function($scope, $rootScope, $stateParams,  $filter, $state, $location){
+var ContactCtrl = GMApp.controller('ContactCtrl', ['$scope', '$rootScope', '$stateParams', '$filter', '$state','$location', '$http', function($scope, $rootScope, $stateParams,  $filter, $state, $location, $http){
     $scope.init = function() {
         setTimeout(function() {
             $('.navigation').addClass('darkHeader');
         }, 500);
         
+    }
+
+    $scope.sendEmail = function() {
+        var postData = {to : "akshay.soni@infoobjects.com", subject : "Test Send Grid", body : "Hi Akshay"};
+        $http({
+            url: 'http://localhost:90/service/common/sendEmail',
+            method: "POST",
+            data: postData,
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here 
+        }).error(function (data, status, headers, config) {
+            //$scope.status = status;
+        });
     }
     $scope.init();
                
