f9437fb72e73139288eea960789769785b439116
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-06-05 23:51:54 +0530

    New Design

--- server/routes/Route.js
+++ server/routes/Route.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     var ctrl = app.controllers.CommonCtrl;
     app.post('/common/sendEmail', ctrl.sendEmail);
-    app.post('/common/processImageRequest', ctrl.processImageRequest);
+    //app.post('/common/processImageRequest', ctrl.processImageRequest);
+    app.get('/common/uploadImage', ctrl.uploadImage)
 };
